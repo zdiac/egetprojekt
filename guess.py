1f025d9464a3ca6b9ebdf567a55a2e81e5dec26f
Elias <endzish@gmail.com>
2026-04-14 13:49:30 +0200

    feat: added the rest of the code

--- guess.py
+++ guess.py
@@ -2,5 +2,18 @@ import random
 
 def main ():
     secret_number = random.randint(1, 100)
+
     while True:
         guess = int(input("Guess a number: "))
+
+        if guess > secret_number:
+            print("Too high!")
+
+        elif guess < secret_number:
+            print("Too low!")
+            
+        else:
+            print("Correct!")
+            break
+
+main()
