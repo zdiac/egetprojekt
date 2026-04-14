da2f56300754893c6498ca43e6e7219cf29d9f0b
Elias <endzish@gmail.com>
2026-04-14 14:45:41 +0200

    fix: fixed code

--- guessinggame.py
+++ guessinggame.py
@@ -1,27 +1,30 @@
 import random
 
-def main ():
+def main():
+    while True:
+        difficulty = input("Choose difficulty (easy, medium, hard or quit): ")
 
-    difficulty = input("Choose difficulty (easy, medium, hard): ")
-    if difficulty == "easy":
-        secret_number = random.randint(1, 50)
-    elif difficulty == "medium":
-        secret_number = random.randint(1, 100)
-    elif difficulty == "hard":
-        secret_number = random.randint(1, 500)
-    else:
-        print("Invalid difficulty, defaulting to medium.")
-        secret_number = random.randint(1, 100)
+        if difficulty == "easy":
+            secret_number = random.randint(1, 50)
+            break
+        elif difficulty == "medium":
+            secret_number = random.randint(1, 100)
+            break
+        elif difficulty == "hard":
+            secret_number = random.randint(1, 500)
+            break
+        elif difficulty == "quit":
+            return
+        else:
+            print("Invalid choice, try again")
 
     while True:
         guess = int(input("Guess a number: "))
 
         if guess > secret_number:
             print("Too high!")
-
         elif guess < secret_number:
             print("Too low!")
-            
         else:
             print("Correct!")
             break
